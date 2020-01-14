bdab06ad783e765eb4775cc4633d962a5ffc4219
PierreGent <pierregentile42@gmail.com>
2020-01-14 17:51:49 +0100

    initialisation

---
+++ R/co_authors_study.R
@@ -0,0 +1,18 @@
+library(igraph)
+my_data <- read.csv('output.csv', header = TRUE, row.names = 1)
+my_matrix <- as.matrix(my_data)
+graph <- graph.adjacency(my_matrix, mode='undirected', diag = FALSE)
+
+
+
+isolated <- which(degree(graph)<15)
+g2 <- delete.vertices(graph, isolated)
+
+isolated2 <- which(degree(g2)==0)
+g3 <- delete.vertices(g2, isolated2)
+
+community <- walktrap.community(g3)
+
+tkplot(community, g3, vertex.label = NA)
+
+plot_dendrogram(community)
