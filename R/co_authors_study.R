ca4b103cd85084e910e1bd274d5d90335ca09aff
fdr <fdr4242@gmail.com>
2020-01-15 16:39:40 +0100

    graph des voisins

--- R/co_authors_study.R
+++ R/co_authors_study.R
@@ -1,18 +1,20 @@
 library(igraph)
 my_data <- read.csv('../generated_data/output.csv', header = TRUE, row.names = 1)
 my_matrix <- as.matrix(my_data)
-graph <- graph.adjacency(my_matrix, mode='undirected', diag = FALSE)
-
-
+graph <- graph.adjacency(weighted=TRUE,my_matrix, mode='undirected', diag = FALSE)
 
 isolated <- which(degree(graph)<15)
 g2 <- delete.vertices(graph, isolated)
 
 isolated2 <- which(degree(g2)==0)
 g3 <- delete.vertices(g2, isolated2)
-
 community <- walktrap.community(g3)
+g3<-g3%>%
+  set_edge_attr("label", value = E(g3)$weight)
+par(cex=0.1)
+E(g3)$label.cex=10
+E(g3)$label.color="red"
 
-plot(community, g3, vertex.label = NA)
-par(cex=0.6)
+plot(community, g3, vertex.label = NA,vertex.size=5)
+par(cex=0.9)
 plot_dendrogram(community)
